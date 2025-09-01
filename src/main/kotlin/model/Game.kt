e9cf5d60b7b4d518f59b204477d30e987551017a
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2025-09-01 18:09:32 -0500

    Update Game to use sealed class GameState as default state

--- src/main/kotlin/model/Game.kt
+++ src/main/kotlin/model/Game.kt
@@ -3,5 +3,5 @@ package com.tennis.scoring.model
 data class Game(
     val player1: Player,
     val player2: Player,
-    var state: GameState = GameState.LOVE_LOVE
+    var state: GameState = GameState.LoveLove
 )
