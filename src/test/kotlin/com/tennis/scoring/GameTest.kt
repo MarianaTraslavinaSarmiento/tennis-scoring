804ede70b996b02bf325ab58980a118c26acc014
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2025-09-01 11:20:39 -0500

    Add test: the score updates correctly when a player scores

--- src/test/kotlin/com/tennis/scoring/GameTest.kt
+++ src/test/kotlin/com/tennis/scoring/GameTest.kt
@@ -11,14 +11,28 @@ class GameTest {
 
     private val scoreService = ScoreService()
 
-    private fun createGame(): Game {
+    private fun newGame(): Game {
         return Game(Player("Player 1"), Player("Player 2"))
     }
 
     @Test
     fun `the initial score of the game is expected to be Love-Love`() {
-        val game = createGame()
+        val game = newGame()
         assertEquals("Love-Love", scoreService.getScore(game))
     }
+
+    @Test
+    fun `the score updates correctly when a player scores`() {
+        val game = newGame()
+        scoreService.pointWonBy(game, "Player 1")
+        assertEquals("15 - Love", scoreService.getScore(game))
+    }
+
+
+
+
+
+
+
 
 }
